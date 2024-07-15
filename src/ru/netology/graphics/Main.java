21a073b3c0775b28fb73401c80434d0a2365b332
ViiRus <ViiRus@Curse.com>
2024-07-15 19:32:18 +0300

    123

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -8,7 +8,7 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
+        TextGraphicsConverter converter = 123 ; // Создайте тут объект вашего класса конвертера
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
